042d604e089318f86096cb02fa0d60e97192cced
perchas04 <166369652+perchas04@users.noreply.github.com>
2025-04-09 12:12:27 +0200

    Update Entornos.java

--- Entornos.java
+++ Entornos.java
@@ -1,3 +1,6 @@
-//Hola soy jorge desde el archivo java en GitHub
-//Hola Soy Jorge Desde La Terminal
-//Este es comentario desde git
+hola hola
+public class prueba {
+    public static void ejemplo(){
+        System.out.println(hola);
+    }
+}
